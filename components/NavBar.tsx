c298d070653e3b82ffc3bfe102977ce6b8310149
Rattapon Apiratjit <krtp.apirat@gmail.com>
2023-11-29 14:54:00 +0700

    bring side menu back.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,12 +3,14 @@ import { buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { getSession, signOut } from "@/server/actions/auth";
 import AuthSection from "./nav/AuthSection";
+import SideMenu from "./SideMenu";
 
 export default async function NavBar() {
   const session = await getSession();
 
   return (
     <nav className="h-14 bg-primary sticky top-0 left-0 right-0 bottom-0 px-3 flex justify-between items-center shadow-md">
+      <SideMenu />
       <Link
         href="/"
         className={cn(
@@ -20,6 +22,7 @@ export default async function NavBar() {
       >
         Sphere Accounts
       </Link>
+      <div className="flex-1" />
       <AuthSection session={session} signOutFn={signOut} />
     </nav>
   );
