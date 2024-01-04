a72caf669614a570feb0ea13ed5da17eb960a965
boraxpr <tata-naipawat@hotmail.com>
2024-01-04 12:37:58 +0700

    testing library and recharts radial+bar charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import { getSession } from "@/server/actions/auth";
+import { getSession } from "../server/actions/auth";
+import { BarChartComponent, RadialChartComponent } from "./client";
+
 export const metadata = {
   title: "Index Page | Sphere Accounts",
 };
@@ -6,21 +8,49 @@ export const metadata = {
 
 export default async function Index() {
   const session = await getSession();
+  const vendorActivity = [
+    { day: 'M', value: 200 },
+    { day: 'T', value: 300 },
+    { day: 'W', value: 150 },
+    { day: 'Th', value: 220 },
+    { day: 'F', value: 250 },
+    { day: 'Sat', value: 75 },
+    { day: 'Sun', value: 22 },
+  ];
+  const sales = [
+    {
+      name: 'Sold',
+      uv: 8000,
+      fill: '#f37221',
+    },
+    {
+      name: 'Produced',
+      uv: 14000,
+      fill: '#3cc061',
+    },
+  ];
+
   return (
     // Logged in 
     (
-      session && <div>
+      session &&
+      <div>
         <div className="py-5 px-20 space-y-1">
           <div className="flex justify-between items-center">
-            <div className="w-7/12 print:w-11/12 mx-5 p-4 m-2 bg-white shadow-md rounded-md border">
+            <div className="w-7/12 print:w-11/12 mx-5 p-4 m-2 bg-#000000 shadow-md rounded-md border">
             </div>
-            <div className="w-5/12 print:w-11/12 mx-5 p-4 m-2 bg-white shadow-md rounded-md border">
+            <div className="w-5/12 print:w-11/12 mx-5 p-10 m-2 bg-white shadow-md rounded-md border">
+              <div className="text-xl font-bold">Vendor Activity</div>
+              <BarChartComponent data={vendorActivity} />
             </div>
           </div>
           <div className="flex justify-between items-center">
             <div className="w-7/12 print:w-11/12 mx-5 p-4 m-2 bg-white shadow-md rounded-md border">
+
             </div>
-            <div className="w-5/12 print:w-11/12 mx-5 p-4 m-2 bg-white shadow-md rounded-md border">
+            <div className="w-5/12 print:w-11/12 mx-5 p-10 m-2 bg-white shadow-md rounded-md border">
+              <div className="text-xl font-bold">Cars Sold</div>
+              <RadialChartComponent data={sales} />
             </div>
           </div>
         </div>
